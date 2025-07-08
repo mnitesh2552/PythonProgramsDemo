9d0eba1d359d9173563bc79410b720819843c251
Nitesh Kumar <mnitesh.0055@gmail.com>
2025-07-08 21:32:09 +0530

    Pushing my first program- Take 2 numbers as input and +,-,* as per user choice

---
+++ Basics/Lab01.py
@@ -0,0 +1,12 @@
+num_1 = float(input('enter number 1= '))
+num_2 = float(input('enter number 2= '))
+
+choice = input('Enter your Choice + - * = ')
+if choice == '+':
+    print(f'addition:{num_1 + num_2}')
+elif choice == '-':
+    print(f'Substraction : {num_1 - num_2}')
+elif choice == '*':
+    print(f'Substraction : {num_1 * num_2}')
+else:
+    print('Invalid Choice')
